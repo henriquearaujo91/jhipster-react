fb290bf19194db506645282ce18d5fb00c42c745
Henrique Araujo <henrique_araujo91@outlook.com>
2022-04-25 22:01:39 -0300

    Initial version of lab generated by generator-jhipster@7.8.1

---
+++ src/main/java/br/com/havs/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package br.com.havs.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Lab.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
